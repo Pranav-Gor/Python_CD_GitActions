90f9c8ae4282c6fa97eeb39291cb24203ea70868
Pranav Gor <gitidpranavgor@gmail.com>
2026-04-07 23:34:44 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -3,10 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 # Addition
-@app.route('/add/<int:a>/<int:b>')
-def add(a, b):
+@app.route('/add')
+def add():
+    a = 10
+    b = 5
     result = a + b
     return f"Addition of {a} and {b} is {result}"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
